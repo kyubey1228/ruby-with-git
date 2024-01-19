d6f8c9142cc314629066b2a3f8af9230d7a1e629
新井楓実奈 <>
2024-01-20 03:01:13 +0900

    N+1について考えた

--- 2024_01_19_b.rb
+++ 2024_01_19_b.rb
@@ -1,5 +1,26 @@
-Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+def n_plus_one(users)
+  users.each do |user|
+    posts = user.posts
+    puts posts.count
+  end
 end
 
+users = User.all
+
+n_plus_one(users)
+
+# ユーザーが取得でき、かつユーザーにbelongしているpostの件数がわかるとしたいが、
+# これはN+1問題が起きる
+
+# ユーザーの数だけpostを引っ張るクエリを発行される
+
+# この問題は、ActiveRecordの includes メソッドを使用することで解決できる
+# includes メソッドは、関連するオブジェクトを事前にロードするために使用され、
+# 必要な全てのデータを一度のクエリで取得できます。
+
+# users = User.includes(:posts).all
+
+# users.each do |user|
+#   posts = user.posts
+#   puts posts.count
+# end
