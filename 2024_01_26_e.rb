cfbc69445b56b95443adbe95948624ccfe551ab3
新井楓実奈 <>
2024-01-26 14:39:46 +0900

    正規表現3

--- 2024_01_26_e.rb
+++ 2024_01_26_e.rb
@@ -5,3 +5,12 @@ puts text.split(/,|-/)
 # gsub,gsub!
 puts text.gsub(',',':')
 puts text.gsub(/,|-/,':')
+
+# hashを渡すことも可能
+hash = { "," => ":", "-"=> "&" }
+puts text.gsub(/,|-/, hash)
+
+puts text.gsub(/,|-/){ |matched| matched == ',' ? ':' : '&' }
+
+text.gsub!(/,|-/,'hogehoge')
+puts text
