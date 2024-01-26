a56590224c2be336153716a8143a4e49b37e01bf
新井楓実奈 <>
2024-01-26 11:50:21 +0900

    インスタンスの確認

--- 2024_01_26_c.rb
+++ 2024_01_26_c.rb
@@ -3,3 +3,7 @@ ar = [1, 'a', 2, 'b', 3, 'c']
 puts ar.select{|i| i.is_a?(Integer) }
 
 # is_a?メソッドは、引数のオブジェクトが指定したクラスのインスタンスかどうかを判定するメソッド
+
+# ハッシュの配列 [{ name: "Alice", age: 30 }, { name: "Bob", age: 20 }] から age が25より大きい要素のみを選択するRubyのコードは？
+h = [{ name: "Alice", age: 30 }, { name: "Bob", age: 20 }]
+puts h.select{|i| i[:age] > 25 }
